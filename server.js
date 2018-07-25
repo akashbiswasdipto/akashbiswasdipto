45a2849946bf92ae351633b8209e3aa89bf24865
Akash Biswas <akashbiswas.dipto@gmail.com>
2018-07-25 04:52:20 +0000

    classwork8

--- server.js
+++ server.js
@@ -1,15 +1,51 @@
 var http = require('http');
-var fs = require('fs');
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('index.html',function(err, data){
-        if(err){
-        return console.log("file read error");
-    }
-    res.end(data);
-  });
- });
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+//for local
+//var db,uri="mongodb://localhost:27071";
+//for c9
+var db,uri="mongodb://"+process.env.IP+":27017";
+
+mongo.MongoClient.connect(uri,
+{userNewUrlParser:true},
+function(err,client){
+  if(err){
+    console.log('could not connect to MongoDB')
+  }else{
+    db= client.db('node-cw8');
+    
+  }
+})
+var save = function(form_data){
+  db.createCollection('users',function (err, collection){});
+  var collection = db.collection('users');
+  collection.save(form_data);
+}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+var server = http.Server(app);
+
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+app.get('/about',function(req,res){
+  res.sendFile(__dirname+'/about.html');
+});
+app.get('/email',function(req,res){
+  res.sendFile(__dirname+'/email.html');
+});
+app.post('/submit_user',function(req,res)
+{
+    console.log(req.body);
+    save(req.body);
+    res.status(200);
+});
+app.get('/about',function(req,res){
+  res.sendFile(__dirname+'/about.html');
+});
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
   });
